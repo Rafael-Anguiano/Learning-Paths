f5e37b6805b9cf2e8baa6f6fe1bfff89a4218832
Rafael Anguiano <rj.anguiano.23@gmail.com>
2024-08-12 19:17:11 -0600

    Route Between Nodes using Breadth First Search

--- Coding-Interview/Trees-Graphs/RouteBetweenNodes.cpp
+++ Coding-Interview/Trees-Graphs/RouteBetweenNodes.cpp
@@ -11,6 +11,7 @@
 
 #include <iostream>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
@@ -29,6 +30,9 @@ public:
     void addNext(Node* node) {
       next.push_back(node);
     }
+    void addNext(vector<Node*> nodes) {
+      this->next = nodes;
+    }
 };
 
 
@@ -37,7 +41,10 @@ public:
   vector<Node*> nodes;
 
   void addNode (Node* node) {
-    nodes.push_back(node);
+    this->nodes.push_back(node);
+  }
+  void addNode (vector<Node*> nodes) {
+    this->nodes = nodes;
   }
 
   bool routeBetweenNodes (Node* start, Node* end) {
@@ -55,32 +62,75 @@ public:
     }
     return false;
   }
+
+  bool breadthFirstSearch (Node* start, Node* end) {
+    if (start == nullptr) return false;
+
+    queue<Node*> q;
+    q.push(start);
+
+    while (!q.empty()) {
+      Node* curr = q.front();
+      q.pop();
+
+      if (curr->visited) continue;
+      cout << "Curr: " << curr->data << endl;
+
+      for (Node* child : curr->next) {
+        if (child->data == end->data) {
+          cout << "END: " << child->data << endl;
+          return true;
+        }
+        if (!child->visited) q.push(child);
+      }
+
+      curr->visited = true;
+    }
+    cout << "Finished" << endl;
+
+    return false;
+  }
 };
 
 
 int main () {
+  Node* zero = new Node(0);
   Node* one = new Node(1);
   Node* two = new Node(2);
   Node* three = new Node(3);
   Node* four = new Node(4);
   Node* five = new Node(5);
   Node* six = new Node(6);
-  one->addNext(two);
-  one->addNext(three);
-  two->addNext(six);
-  three->addNext(four);
-  five->addNext(one);
-  six->addNext(four);
+  Node* seven = new Node(7);
+  Node* eight = new Node(8);
+  Node* nine = new Node(9);
+  Node* ten = new Node(10);
+
+  /**
+    * Conections
+    */
+  zero->addNext({five, nine});
+  one->addNext({five, six});
+  // two no have next
+  three->addNext({zero});
+  four->addNext({two});
+  five->addNext({zero});
+  six->addNext({four});
+  seven->addNext({one, two, six});
+  eight->addNext({zero, three});
+  nine->addNext({four});
+  ten->addNext({four, eight});
+
 
   Graph* graph = new Graph();
-  graph->addNode(one);
-  graph->addNode(two);
-  graph->addNode(three);
-  graph->addNode(four);
-  graph->addNode(five);
-  graph->addNode(six);
+  graph->addNode({
+    zero, one, two, three, four, five, six, seven, eight, nine, ten
+  });
 
-  cout << graph->routeBetweenNodes(five, six) << endl;
+  cout << graph->routeBetweenNodes(seven, five) << endl;
+  
+  cout << "Breadth Firts Search " << endl; 
+  cout << graph->breadthFirstSearch(seven, three) << endl;
 
   return 0;
 }
