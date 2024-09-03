7d7aa71dcb0e188168c006eb65eb22b477e146c5
Rafael Anguiano <rj.anguiano.23@gmail.com>
2024-09-03 13:25:44 -0600

    Flip To Win
    You have an integer and you can flip exactly one bit from a 0 to a 1.
    Write code to find the length of the longest sequence of 1s you could
    create.

---
+++ Coding-Interview/Bit-Manipulation/FlipToWin.cpp
@@ -0,0 +1,56 @@
+/**
+* BIT MANIPULATION
+* Problem: Flip to Win
+* You have an integer and you can flip exactly one bit from a 0 to a 1.
+* Write code to find the length of the longest sequence of 1s you could create.
+*
+* EXAMPLE
+* Input: 1775 (or: 11011101111)
+* Output: 8
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool getBit (int num, int i) {
+  return num & (1<<i);
+}
+
+
+int flipToWin (int number) {
+  vector<int> consecutive;
+  int curr = 0;
+  int max = 0;
+
+  for (int i=0; i<32; i++) {
+    curr++;
+    if (getBit(number, i+1) != getBit(number, i)) {
+      consecutive.push_back(getBit(number, i) ? curr : curr * -1);
+      if (getBit(number, i) && curr > max) max = curr;
+      curr = 0;
+    }
+  }
+
+  for (int i=0; i<consecutive.size()-2; i++) {
+    if (consecutive[i] < 0) continue;
+
+    if (consecutive[i+1] == -1) {
+      if (consecutive[i+2] + consecutive[i] + 1 > max)
+        max = consecutive[i+2] + consecutive[i] + 1;
+    }
+  }
+  return max;
+}
+
+int main () {
+  
+  int number = 2005;
+
+  cout << flipToWin(number) << endl;
+
+
+  return 0;
+}
